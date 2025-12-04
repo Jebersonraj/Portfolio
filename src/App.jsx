16760860f037282c064b312f984d31f3f235ce34
Jeberson raj <jebersonraj7@gmail.com>
2025-12-04 11:29:49 +0530

    refactor App component layout for improved section organization

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,35 @@ import Header from "./components/Header/Header.jsx";
 import TopContainer from "./components/TopContainer/TopContainer.jsx";
 import Skills from "./components/skills/Skills.jsx";
 import ProjectContainer from "./components/project-container/Project-container.jsx";
-import "./App.css"
 import Education from "./components/Education/Education.jsx";
 import Achievements from "./components/Achivements/Achivements.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import Joinwithme from "./components/Joinwithme/Joinwithme.jsx";
+import "./App.css"
 
 function App() {
     return (
         <div className="Home">
             <Header/>
             <TopContainer/>
-            <Skills/>
-            <ProjectContainer/>
-            <Education/>
-            <Achievements/>
-            <Contact/>
-            <Joinwithme/>
+            <div className="section-layer-top">
+                <Skills/>
+            </div>
+            <div className="section-layer-bottom">
+                <ProjectContainer/>
+            </div>
+            <div className="section-layer-top">
+                <Education/>
+            </div>
+            <div className="section-layer-bottom">
+                <Achievements/>
+            </div>
+            <div className="section-layer-top">
+                <Contact/>
+            </div>
+            <div className="section-layer-bottom">
+                <Joinwithme/>
+            </div>
         </div>
     );
 }
